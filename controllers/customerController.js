5d1874736fee812a88d03362dbd45a62fd136680
Rizal Firmansyah <rizalfirmansyah2105@gmail.com>
2024-10-17 19:02:34 +0700

    add customersCreatePage in customerController

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -3,7 +3,7 @@ const { Customer } = require("../models");
 async function showCustomerPage(req, res) {
   try {
     const customers = await Customer.findAll();
-     res.render("pages/customers", {
+    res.render("pages/customers/index", {
       customers,
       layout: "layouts/main-layout",
       title: "Customers Page",
@@ -11,15 +11,47 @@ async function showCustomerPage(req, res) {
       scriptFile: "customers.js",
       currentPage: "customers",
     });
-  } catch (error) {
-    console.error(error);
+  } catch (err) {
+    console.error(err);
     res.status(500).json({
       status: "Failed",
       message: "Failed to show page",
       isSuccess: false,
-      error: error.message,
+      error: err.message,
+    });
+  }
+}
+async function createCustomerPage(req, res) {
+  try {
+    res.render("pages/customers/customers", {
+      layout: "layouts/main-layout",
+      title: "Customers Create Page",
+      styleFile: "customers.css",
+      scriptFile: "customers.js",
+      currentPage: "customers",
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      status: "Failed",
+      message: "Failed to create show page",
+      isSuccess: false,
+      error: err.message,
     });
   }
 }
 
-module.exports = { showCustomerPage };
+// const createCustomer = async (req, res) => {
+//   try {
+//     const { firstName, lastName, email, address } = req.body;
+//     await Customer.create({ firstName, lastName, email, address });
+//     req.flash("message", "Customer added successfully!");
+//     res.redirect("/customers");
+//   } catch (err) {
+//     console.error(err.message);
+//     req.flash("error", "Failed to add customer");
+//     res.redirect("/customers/create");
+//   }
+// };
+
+module.exports = { showCustomerPage, createCustomerPage };

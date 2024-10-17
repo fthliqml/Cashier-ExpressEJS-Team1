a1418d75f5007f412a7262c0151758d25cdb25a9
Ferdi Firdaus Ega Pratama <ferdifirdaus157@gmail.com>
2024-10-17 10:51:52 +0700

    first commit

--- app.js
+++ app.js
@@ -10,23 +10,30 @@ configureMiddleware(app);
 
 // health check
 app.get("/", (req, res) => {
-    try {
-        res.status(200).json({
-            status: "Success",
-            message: "Ping successfully",
-            isSuccess: true,
-        });
-    } catch (error) {
-        res.status(500).json({
-            status: "Failed",
-            message: "Ping failed",
-            isSuccess: false,
-            error: error.message,
-        });
-    }
+  try {
+    res.status(200).json({
+      status: "Success",
+      message: "Ping successfully",
+      isSuccess: true,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "Failed",
+      message: "Ping failed",
+      isSuccess: false,
+      error: error.message,
+    });
+  }
 });
+
+// Middleware untuk mengizinkan parsing data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const customerRoutes = require("./routes/customerRoute");
+app.use("/customers", customerRoutes);
 
 const PORT = process.env.PORT_NUMBER;
 app.listen(PORT, () => {
-    console.log(`App running on http://localhost:${PORT}`);
+  console.log(`App running on http://localhost:${3000}`);
 });

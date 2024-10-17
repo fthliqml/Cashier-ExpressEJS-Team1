ba2aa0a1d0e448ddbbd1818dc55b26157b248bf7
Aqsaprima <aqsaprima11@gmail.com>
2024-10-17 15:04:34 +0700

    add create order page

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,22 +1,45 @@
 function showOrderPage(req, res) {
-    try {
-        // Rendering file with template engines (ejs)
-        res.render("pages/order", {
-            layout: "layouts/main-layout",
-            title: "Order",
-            styleFile: "order.css",
-            scriptFile: "order.js",
-            currentPage: "orders",
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({
-            status: "Failed",
-            message: "Failed to show page",
-            isSuccess: false,
-            error: error.message,
-        });
-    }
+  try {
+    // Rendering file with template engines (ejs)
+    res.render("pages/orders", {
+      layout: "layouts/main-layout",
+      title: "Order",
+      styleFile: "orders/order.css",
+      scriptFile: "order.js",
+      currentPage: "orders",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      status: "Failed",
+      message: "Failed to show page",
+      isSuccess: false,
+      error: error.message,
+    });
+  }
 }
 
-module.exports = { showOrderPage };
+async function createPage(req, res) {
+  try {
+    res.render("pages/orders/create", {
+      layout: "layouts/main-layout",
+      title: "Order",
+      styleFile: "orders/create.css",
+      scriptFile: "",
+      currentPage: "orders",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      status: "Failed",
+      message: "Failed to show page",
+      isSuccess: false,
+      error: error.message,
+    });
+  }
+}
+
+module.exports = {
+  showOrderPage,
+  createPage,
+};

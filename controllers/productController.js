9d0d87c4c27f471f0fc7ff2ffa8efa2bc8a3745e
unknown <almagiusunggane@gmail.com>
2024-10-19 12:02:58 +0700

    Add create product

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,22 +1,60 @@
+const { Product } = require("../models");
+
 function showProductPage(req, res) {
-    try {
-        // Rendering file with template engines (ejs)
-        res.render("pages/products", {
-            layout: "layouts/main-layout",
-            title: "Products",
-            styleFile: "products.css",
-            scriptFile: "products.js",
-            currentPage: "products",
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({
-            status: "Failed",
-            message: "Failed to show page",
-            isSuccess: false,
-            error: error.message,
-        });
-    }
+  try {
+    // Rendering file with template engines (ejs)
+    res.render("pages/products", {
+      layout: "layouts/main-layout",
+      title: "Products",
+      styleFile: "products.css",
+      scriptFile: "products.js",
+      currentPage: "products",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      status: "Failed",
+      message: "Failed to show page",
+      isSuccess: false,
+      error: error.message,
+    });
+  }
 }
 
-module.exports = { showProductPage };
+async function createProduct(req, res) {
+  const newProduct = req.body;
+  console.log(newProduct);
+  try {
+    await Product.create(newProduct);
+    res.redirect("/products");
+  } catch (error) {
+    console.error(error.message);
+    res.redirect("/products/create");
+  }
+}
+
+async function createProductPage(req, res) {
+  try {
+    res.render("pages/products/create", {
+      layout: "layouts/main-layout",
+      title: "Add Product",
+      styleFile: "products.css",
+      scriptFile: "products.js",
+      currentPage: "products",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      status: "Failed",
+      message: "Failed to show page",
+      isSuccess: false,
+      error: error.message,
+    });
+  }
+}
+
+module.exports = {
+  showProductPage,
+  createProductPage,
+  createProduct,
+};
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const { productController } = require("../controllers");
 
 router.get("/products", productController.showProductPage);
+router.get("/products/create", productController.createProductPage);
+router.post("/products/create", productController.createProduct);
 
 module.exports = router;

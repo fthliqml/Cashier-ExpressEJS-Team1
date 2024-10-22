cf8ecea186eed9262c6fca467121aa436de59bc8
Ananda Darma <anandadarma2612@gmail.com>
2024-10-23 01:04:37 +0800

    create new delete api for review page and fixing ejs of review page

--- routes/reviewRoute.js
+++ routes/reviewRoute.js
@@ -7,6 +7,8 @@ router.get("/reviews", reviewController.showReviewPage);
 router.get("/reviews/chooseOrder", reviewController.chooseOrderReviewPage);
 router.post("/reviews/chooseOrder", reviewController.createReviewPage);
 router.post("/reviews/create", reviewController.createReview);
+router.post("/reviews/delete/:id", reviewController.deleteReview);
+
 // router.get("/reviews/:id", reviewController.showReviewPage);
 
 module.exports = router;

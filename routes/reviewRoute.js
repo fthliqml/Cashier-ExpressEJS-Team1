9a5b24bf4995cb2c099f84dd513f7a08331bdffd
Ananda Darma <anandadarma2612@gmail.com>
2024-10-23 16:38:58 +0800

    create update api for edit data at review page

--- routes/reviewRoute.js
+++ routes/reviewRoute.js
@@ -7,6 +7,7 @@ router.get("/reviews", reviewController.showReviewPage);
 router.get("/reviews/chooseOrder", reviewController.chooseOrderReviewPage);
 router.post("/reviews/chooseOrder", reviewController.createReviewPage);
 router.post("/reviews/create", reviewController.createReview);
+router.post("/reviews/update/:id", reviewController.updateReview);
 router.post("/reviews/delete/:id", reviewController.deleteReview);
 
 // router.get("/reviews/:id", reviewController.showReviewPage);
